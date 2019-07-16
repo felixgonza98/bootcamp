cbfc2d1164893c30f18608a63994e6471f60aaa0
Felix Gonzalez <felixgonza1540@gmail.com>
2019-07-16 15:33:49 -0400

    Programacion orientadas en objetos

---
+++ clases/personas.py
@@ -0,0 +1,24 @@
+
+class Persona :
+    edad = 5
+
+    def __init__(self, un_nombre):
+        self.mi_nombre = un_nombre
+        print("Hola naci, me llamo", self.mi_nombre)
+
+    def cumple(self):
+        self.edad = self.edad + 1
+    
+    def restaedad(self):
+        self.edad = self.edad - 1
+
+pepe = Persona("Pepito")
+pepa = Persona("Pepita")
+print(pepe.edad)
+print(pepe.mi_nombre)
+
+pepe.cumple()
+print(pepe.edad)
+
+pepe.restaedad()
+print(pepe.edad)
